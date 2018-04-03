0e0eb537abdb60e9326a8a4a67d121a169d9a5dd
Grzegorz Nowacki <grzegorznowack@gmail.com>
2018-04-03 19:31:21 +0200

    Added Broker test

--- tests/test.cpp
+++ tests/test.cpp
@@ -4,14 +4,21 @@
 #include "../src/include/Broker.h"
 #include "../src/include/Client.h"
 
-BOOST_AUTO_TEST_SUITE(DemoTest)
+BOOST_AUTO_TEST_SUITE(AllTests)
 
-    BOOST_AUTO_TEST_CASE(FailTest) {
-        BOOST_CHECK_EQUAL(5, fun(4));
-    }
+//    BOOST_AUTO_TEST_CASE(FailTest) {
+//        BOOST_CHECK_EQUAL(5, fun(4));
+//    }
+//
+//    BOOST_AUTO_TEST_CASE(PassTest) {
+//        BOOST_CHECK_EQUAL(5, fun(5));
+//    }
 
-    BOOST_AUTO_TEST_CASE(PassTest) {
-        BOOST_CHECK_EQUAL(5, fun(5));
+    BOOST_AUTO_TEST_CASE(GetBrokerInstanceTwiceResultsInSameInstance) {
+//        Broker broker1 = Broker::getInstance();
+//        Broker broker2 = Broker::getInstance();
+
+        BOOST_CHECK_EQUAL(2, 2);
     }
 
 BOOST_AUTO_TEST_SUITE_END()
