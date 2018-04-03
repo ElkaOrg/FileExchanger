ab353df1ca115e549d811fda65b852b592d480ac
Grzegorz Nowacki <grzegorznowack@gmail.com>
2018-04-03 13:12:04 +0200

    Change CMakeLists files

---
+++ tests/test.cpp
@@ -0,0 +1,16 @@
+#define BOOST_TEST_MODULE ProgramTests
+#include <boost/test/unit_test.hpp>
+
+#include "../src/include/Broker.h"
+#include "../src/include/Client.h"
+
+
+BOOST_AUTO_TEST_CASE(FailTest)
+{
+    BOOST_CHECK_EQUAL(5, fun(4));
+}
+
+BOOST_AUTO_TEST_CASE(PassTest)
+{
+    BOOST_CHECK_EQUAL(5, fun(5));
+}
