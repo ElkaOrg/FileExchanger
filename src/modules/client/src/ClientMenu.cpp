f45e4b9486748702193e9beb8f170b9aa0172a6f
Grzegorz Nowacki <grzegorznowack@gmail.com>
2018-05-07 15:20:35 +0200

    Addee switch

--- src/modules/client/src/ClientMenu.cpp
+++ src/modules/client/src/ClientMenu.cpp
@@ -5,5 +5,37 @@
 
 void ClientMenu::showMainMenu(void)
 {
-    std::cout<<"Choose directory to be shared"<<std::endl;
+    int chosenNumber = 0;
+    std::cout<<"0 - Quit"<<std::endl;
+    std::cout<<"1 - Choose directory to be shared"<<std::endl;
+    //TODO use case
+
+    std::cin>>chosenNumber;
+
+    while(1)
+    {
+        switch (chosenNumber) {
+            case 0:
+                std::cout << "Exiting..." << std::endl;
+                //TODO do clean up
+                break;
+            case 1:
+                std::cout << "Option 1" << std::endl;
+                break;
+
+            case 2:
+                //jakiś kod
+                break;
+
+                //...
+            case 3:
+                //jakiś kod
+                break;
+
+            default:
+                std::cout << "Error has occured" << std::endl;
+                exit(0);
+                break;
+        }
+    }
 }
