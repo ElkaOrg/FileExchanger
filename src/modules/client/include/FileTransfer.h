52170caf6b0b4418e3bba42e5ecbce50c48a15db
Grzegorz Nowacki <grzegorznowack@gmail.com>
2018-05-07 16:00:48 +0200

    Added FileTransfer class

---
+++ src/modules/client/include/FileTransfer.h
@@ -0,0 +1,16 @@
+//
+// Created by gnowacki on 07.05.18.
+//
+
+#ifndef FILEEXCHANGER_FILETRANSFER_H
+#define FILEEXCHANGER_FILETRANSFER_H
+
+#include "../../../include/inc.h"
+
+class FileTransfer
+{
+public:
+    int sendOneFile(char* filePath);
+};
+
+#endif //FILEEXCHANGER_FILETRANSFER_H
---
+++ src/modules/client/src/FileTransfer.cpp
@@ -0,0 +1,10 @@
+//
+// Created by gnowacki on 07.05.18.
+//
+
+#include "../include/FileTransfer.h"
+
+int FileTransfer::sendOneFile(char *filePath)
+{
+    
+}
